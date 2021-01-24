7568eabb9c9354a52172b504ca6e84362195e390
HGimself <hgmaxwellking@gmail.com>
2021-01-24 11:56:42 -0500

    this is killing

--- www/deploy.sh
+++ www/deploy.sh
@@ -2,3 +2,4 @@ echo "deploying the client to dockerhub!"
 
 echo $DOCKER_HUB_PASSWORD | docker login -u $DOCKER_HUB_USERNAME --password-stdin
 docker image build -t $DOCKER_HUB_USERNAME/covid-data-dashboard-frontend .
+docker push $DOCKER_HUB_USERNAME/covid-data-dashboard-frontend
